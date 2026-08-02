8e853be811fe249f2306f5aa2222ca90016460ed
Ahmed Hadi <ahmedhadidev@gmail.com>
2026-08-02 18:21:18 +0300

    fix message input

--- script.js
+++ script.js
@@ -63,16 +63,7 @@ document.getElementById("loginBtn");
 
 const errorMessage =
 document.getElementById("errorMessage");
-messageInput.addEventListener(
-"input",
-()=>{
 
-    messageInput.style.height="auto";
-
-    messageInput.style.height =
-    messageInput.scrollHeight + "px";
-
-});
 const savedUser =
 localStorage.getItem("secret_username");
 
@@ -94,6 +85,20 @@ document.getElementById("messageInput");
 const sendBtn =
 document.getElementById("sendBtn");
 
+
+
+// تكبير حقل الرسالة تلقائياً
+
+messageInput.addEventListener(
+"input",
+()=>{
+
+    messageInput.style.height = "auto";
+
+    messageInput.style.height =
+    messageInput.scrollHeight + "px";
+
+});
 
 
 
